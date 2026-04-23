7842c23b70cadf12236f643c2a1ecf56c6cea583
Jeffrey Bos <jeffreybos@users.noreply.github.com>
2026-04-23 20:14:53 +0200

    expand game list to max 7 per category

--- src/games.js
+++ src/games.js
@@ -1,9 +1,9 @@
 // src/games.js
 export const DEFAULT_GAMES = [
-  { cat: '🔫 Shooters', games: ['Counter-Strike 1.6 / CS2', 'Medal of Honor: Allied Assault', 'Unreal Tournament 2004', 'Battlefield 1942', 'Wolfenstein: Enemy Territory'] },
-  { cat: '⚔️ RTS',      games: ['Age of Empires II', 'StarCraft: Brood War', 'Warcraft III', 'Command & Conquer: Generals', 'Rise of Nations'] },
-  { cat: '🏎️ Racen',    games: ['Trackmania Nations', 'Need for Speed: Underground 2', 'Need for Speed: Most Wanted', 'FlatOut 2', 'Rallisport Challenge 2'] },
-  { cat: '⚽ Sport',    games: ['FIFA 2002–2006', 'NBA Street Vol. 2'] },
-  { cat: '💣 Party',    games: ['Worms Armageddon', 'Bomberman', 'Duck Game'] },
-  { cat: '🧙 Co-op',    games: ['Diablo II', 'Neverwinter Nights', 'Gauntlet: Dark Legacy', 'Dungeon Siege'] },
+  { cat: '🔫 Shooters', games: ['Counter-Strike 1.6 / CS2', 'Medal of Honor: Allied Assault', 'Unreal Tournament 2004', 'Battlefield 1942', 'Wolfenstein: Enemy Territory', 'Halo: Custom Edition', 'GTA 4 (GTA Connected)'] },
+  { cat: '⚔️ RTS',      games: ['Age of Empires II', 'StarCraft: Brood War', 'Warcraft III', 'Command & Conquer: Generals', 'Rise of Nations', 'Age of Mythology', 'Company of Heroes'] },
+  { cat: '🏎️ Racen',    games: ['Trackmania Nations', 'Need for Speed: Underground 2', 'Need for Speed: Most Wanted', 'FlatOut 2', 'Rallisport Challenge 2', 'Mario Kart (Dolphin)', 'Wreckfest'] },
+  { cat: '⚽ Sport',    games: ['FIFA 2002–2006', 'NBA Street Vol. 2', 'Rocket League'] },
+  { cat: '💣 Party',    games: ['Worms Armageddon', 'Bomberman', 'Duck Game', 'Golf With Your Friends', 'Blur', 'Garry\'s Mod'] },
+  { cat: '🧙 Co-op',    games: ['Diablo II', 'Neverwinter Nights', 'Gauntlet: Dark Legacy', 'Dungeon Siege', 'Barotrauma', 'Project Zomboid', 'Left 4 Dead 2'] },
 ]
